3bbf9be75786e8d46307ad494ed9039e3fccb02d
greyes97 <greyesl1@miumg.edu.gt>
2021-05-01 19:31:47 -0600

    First commit added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/cientificCalc/CientificCalcApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.cientificCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CientificCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CientificCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/cientificCalc/CientificCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.cientificCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CientificCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
